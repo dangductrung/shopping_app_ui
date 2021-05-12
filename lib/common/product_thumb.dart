5c5cb60b74d6212f607da1b4d6a1928481bdf5d3
Nguyen Hoang Ngoc Tran <nghngtran68@gmail.com>
2021-05-12 22:58:00 +0700

    homepage

--- lib/common/product_thumb.dart
+++ lib/common/product_thumb.dart
@@ -2,36 +2,49 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_app/theme/ui_text_style.dart';
 
-class ProductThumb extends StatelessWidget{
+class ProductThumb extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height/100;
-    final width = MediaQuery.of(context).size.width/100;
-    return Container(margin: EdgeInsets.symmetric(horizontal: 10,vertical: 15),
-      child: Stack(
-        children:[
-          Container(
-            decoration: BoxDecoration(color:Colors.white,
-                border: Border.all(color: Colors.white10,width: 2.0)),
-            width: 180,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisSize: MainAxisSize.max,
+    final height = MediaQuery.of(context).size.height / 100;
+    final width = MediaQuery.of(context).size.width / 100;
+    return Container(
+      margin: EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+      child: Stack(children: [
+        Container(
+          decoration: BoxDecoration(color: Colors.white, border: Border.all(color: Colors.white10, width: 2.0)),
+          width: 180,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisSize: MainAxisSize.max,
             children: [
-                Container(margin: EdgeInsets.only(top:10),
-                  child:Image.asset("assets/imgs/meow.jpg"),width: 120, height: 120,),
-                Expanded(
-                  child: Container(padding: EdgeInsets.fromLTRB(10, 5, 0, 0),
-                    child: Column(
-                        children: [
-                          Text("Bình Hoa Thủy Tinh ", style: UITextStyle.mediumBlack_14_w700),
-                          Container(margin: EdgeInsets.symmetric(vertical: 4),
-                            child: Text("13.999.000", style: UITextStyle.red_16_w700),
-                          ),
-                    ]),
-                    ))])),
-          Container(alignment: Alignment.center,
-            color: Colors.yellow, width: width*10,height: height*5,
+              Container(
+                margin: EdgeInsets.only(top: 10),
+                child: Image.asset("assets/imgs/meow.jpg"),
+                width: 120,
+                height: 120,
+              ),
+              Expanded(
+                child: Container(
+                  padding: EdgeInsets.fromLTRB(10, 5, 0, 0),
+                  child: Column(
+                    children: [
+                      Text("Bình Hoa Thủy Tinh ", style: UITextStyle.mediumBlack_14_w700),
+                      Container(
+                        margin: EdgeInsets.symmetric(vertical: 4),
+                        child: Text("13.999.000", style: UITextStyle.red_16_w700),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Container(
+            alignment: Alignment.center,
+            color: Colors.yellow,
+            width: width * 10,
+            height: height * 5,
             child: Text("-40%", style: TextStyle(fontWeight: FontWeight.w700))),
       ]),
     );
--- lib/pages/home/home.dart
+++ lib/pages/home/home.dart
@@ -3,10 +3,11 @@ import 'package:flutter/material.dart';
 import 'package:shopping_app/common/product_thumb.dart';
 import 'package:shopping_app/common/searchbar.dart';
 
-class HomeScreen extends StatefulWidget{
+class HomeScreen extends StatefulWidget {
   HomeScreenState createState() => HomeScreenState();
 }
-class HomeScreenState extends State<HomeScreen>{
+
+class HomeScreenState extends State<HomeScreen> {
   TextEditingController _controller;
   FocusNode _focusNode;
   String _terms = '';
@@ -32,37 +33,38 @@ class HomeScreenState extends State<HomeScreen>{
   }
 
   Widget _buildSearchBox() {
-    final width = MediaQuery.of(context).size.width/100;
+    final width = MediaQuery.of(context).size.width / 100;
     return Padding(
       padding: const EdgeInsets.all(10),
-      child: Row(mainAxisSize: MainAxisSize.max,
-          children: [
+      child: Row(mainAxisSize: MainAxisSize.max, children: [
         Container(
-          width: width*80,
+          width: width * 80,
           child: SearchBar(
             controller: _controller,
             focusNode: _focusNode,
           ),
         ),
-        Expanded(
-            child: Icon(Icons.notifications, size: 30, color: Colors.white))
+        Expanded(child: Icon(Icons.notifications, size: 30, color: Colors.white))
       ]),
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    final height = MediaQuery.of(context).size.height/100;
-    final width = MediaQuery.of(context).size.width/100;
+    final height = MediaQuery.of(context).size.height / 100;
+    final width = MediaQuery.of(context).size.width / 100;
     return Scaffold(
-      appBar: AppBar(automaticallyImplyLeading: false,
+      appBar: AppBar(
+          automaticallyImplyLeading: false,
           flexibleSpace: Container(
             padding: EdgeInsets.fromLTRB(5, 14, 0, 0),
             decoration: BoxDecoration(
-                gradient: LinearGradient(
-                    begin: Alignment.topRight,
-                    end: Alignment.bottomLeft,
-                    colors: [Color(0xffF12711), Color(0xffF5AF19)])),
+              gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [Color(0xffF12711), Color(0xffF5AF19)],
+              ),
+            ),
             child: Column(
               children: [
                 _buildSearchBox(),
@@ -70,38 +72,44 @@ class HomeScreenState extends State<HomeScreen>{
             ),
           )),
       body: SafeArea(
-        child: Column(mainAxisSize: MainAxisSize.max,
+        child: Column(
+          mainAxisSize: MainAxisSize.max,
           children: [
-            Container(margin: EdgeInsets.symmetric(vertical: 0, horizontal: 0),
-                child:
-            Image.asset("assets/imgs/banner.png"),width: width*200, height: height*16),
-          Container(
-              width: width*100,
-              height: height*39,
-              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
-              color: Colors.red.shade900,
-              child:
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  Container(
-                      child: Text("Sản phẩm nổi bật",
-                          style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500,
-                          color: Colors.white, letterSpacing: 0.8))),
-                  Container(
-                    height: height*30,
-                    child: ListView.builder(scrollDirection: Axis.horizontal,
-                        itemCount: 10,itemBuilder: (context,index){
-                      return ProductThumb();
-                    })),
-                ],
-              )
-          )
+            Container(
+                margin: EdgeInsets.symmetric(vertical: 0, horizontal: 0),
+                child: Image.asset("assets/imgs/banner.png"),
+                width: width * 200,
+                height: height * 16),
+            Container(
+                width: width * 100,
+                height: height * 39,
+                padding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                color: Colors.red.shade900,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Container(
+                      child: Text(
+                        "Sản phẩm nổi bật",
+                        style: TextStyle(fontSize: 16, fontWeight: FontWeight.w500, color: Colors.white, letterSpacing: 0.8),
+                      ),
+                    ),
+                    Container(
+                      height: height * 30,
+                      child: ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 10,
+                        itemBuilder: (context, index) {
+                          return ProductThumb();
+                        },
+                      ),
+                    ),
+                  ],
+                ))
           ],
         ),
       ),
     );
   }
 }
-
